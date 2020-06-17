dbf2c0364e762e0ec46ac89584eb26ccb2c02dbd
gigalexx@gmail.com <!Korovka1361>
2020-06-17 14:03:52 -0700

    98. Validate Binary Search Tree -- Leetcode

---
+++ src/problems/medium/ValidateBinarySearchTree.kt
@@ -0,0 +1,21 @@
+package problems.medium
+
+import Algorithms.TreeNode
+
+fun main() {
+
+}
+
+fun isValidBST(root: TreeNode?): Boolean {
+
+    return isBST(root, Long.MIN_VALUE..Long.MAX_VALUE)
+}
+
+fun isBST(node: TreeNode?, range: LongRange): Boolean {
+    if (node == null) return true
+
+    if (node.value <= range.first || node.value >= range.last) return false
+
+
+    return isBST(node.left, range.first..node.value) && isBST(node.right, node.value..range.last)
+}
