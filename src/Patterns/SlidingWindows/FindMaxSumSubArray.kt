d3f7859e0612781e7fea73f57ac2afb20eaa377d
gigalexx@gmail.com <!Korovka1361>
2020-06-26 12:31:30 -0700

    53. Maximum Subarray (sliding window)

--- src/Patterns/SlidingWindows/FindMaxSumSubArray.kt
+++ src/Patterns/SlidingWindows/FindMaxSumSubArray.kt
@@ -3,23 +3,20 @@ package Patterns.SlidingWindows
 fun main() {
 
 
-    findMaxSumSubArray(intArrayOf(2, 3, 4, 1, 5), 2)
+    print(findMaxSumSubArray(intArrayOf(2, 3, 4, 1, 5), 2))
 
 }
 
 fun findMaxSumSubArray(arr: IntArray, K: Int): Int {
-    var maxSum = 0
+    var maxSum = -1
     var left = 0
     var right = 0
     var currMax = 0
 
     while (right < arr.size) {
         currMax += arr[right]
-
         if (right - left >= K - 1) {
-            if (maxSum < currMax) {
-                maxSum = currMax
-            }
+            maxSum = kotlin.math.max(maxSum,currMax)
             currMax -= arr[left]
             left++
         }
