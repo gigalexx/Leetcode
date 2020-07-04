b9d3bca6699c9ba79cf194ea232ced50670e6a98
gigalexx@gmail.com <!Korovka1361>
2020-07-03 20:28:54 -0700

    update comments

--- src/problems/medium/4Sum.kt
+++ src/problems/medium/4Sum.kt
@@ -1,23 +1,33 @@
 package problems.medium
-//https://leetcode.com/problems/4sum/submissions/
-//18. 4Sum
-//Medium
-//Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
-//
-//Note:
-//
-//The solution set must not contain duplicate quadruplets.
-//
-//Example:
-//
-//Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
-//
-//A solution set is:
-//[
-//[-1,  0, 0, 1],
-//[-2, -1, 1, 2],
-//[-2,  0, 0, 2]
-//]
+/*
+https://leetcode.com/problems/4sum/submissions/
+
+18. 4Sum
+Medium
+Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+
+The solution set must not contain duplicate quadruplets.
+
+Example:
+
+Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+[-1,  0, 0, 1],
+[-2, -1, 1, 2],
+[-2,  0, 0, 2]
+]
+
+Time complexity #
+Sorting the array will take O(N*logN). Overall searchQuadruplets() will take O(N * logN + N^3), which is asymptotically equivalent to O(N^3).
+
+Space complexity #
+The space complexity of the above algorithm will be O(N) which is required for sorting.
+
+ */
 fun main(){
 
     fourSum(intArrayOf(1,0,-1,0,-2,2),0)
