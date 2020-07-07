408db8e0038d8d45b7d768aa3dcb23f3058beb46
gigalexx@gmail.com <!Korovka1361>
2020-07-07 16:03:49 -0700

    876. Middle of the Linked List (slow and fast pointers approach)

--- src/problems/easy/MiddleOfTheLinkedList.kt
+++ src/problems/easy/MiddleOfTheLinkedList.kt
@@ -1,5 +1,28 @@
 package problems.easy
 
+/*
+876. Middle of the Linked List
+Easy
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+Example 2:
+
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+Note:
+The number of nodes in the given list will be between 1 and 100.
+ */
+
 fun main() {
 
     val head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6, null))))))
@@ -8,24 +31,15 @@ fun main() {
 }
 
 
-fun middleNode(head: ListNode?): ListNode? {
+fun middleNode(head: ListNode?): ListNode? {   // TC -> O(N) // SC -> O(1)
 
-    var listSize = 0
+    var fast  = head
+    var slow = head
 
-    var currNode = head
+    while (fast != null && fast.next != null){
+        fast = fast?.next?.next
+        slow = slow?.next
 
-    while (currNode != null) {
-        listSize++
-        currNode = currNode.next
     }
-
-    var midNode = if (listSize % 2 == 0) listSize / 2 else listSize / 2 + 1
-
-    currNode = head
-
-    while (--midNode > 0) {
-        currNode = currNode?.next
-    }
-
-    return currNode
+    return slow
 }
